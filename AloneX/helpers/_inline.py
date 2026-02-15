42117f93f1af1d79cccb249f9c21269e2c3521a9
Thaw Zin (mm) <tzkgaming2019@gmail.com>
2026-02-15 14:56:57 +0630

    newss1

--- AloneX/helpers/_inline.py
+++ AloneX/helpers/_inline.py
@@ -154,7 +154,7 @@ class Inline:
                     self.ikb(text=lang["aloneowner"], user_id=config.OWNER_ID),
                     self.ikb(
                         text=lang["source"],
-                        url="https://github.com/TeamAloneOp/AloneX",
+                        url="https://t.me/HANTHAR_1999",
                     )
                 ]
             ]
